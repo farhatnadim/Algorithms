60f1b57de8299f2acce65c3ecf4a73ddf6231806
nudigamma <nadim.farhat@gmail.com>
2024-04-11 15:37:26 -0400

    Progress in implementing the algorithm

--- MinimumCut/MinimumCut.py
+++ MinimumCut/MinimumCut.py
@@ -5,31 +5,39 @@ import random
 
 # Define the graph
 small_graph = {'1': [2, 3], '2': [1, 3, 4], '3': [1, 2, 4], '4': [2, 3]}
-vertices = ['1','2','3','4']
-edges = [['1','3'],['1','2'],['2','3'],['2','4'],['4','3']]
+
 
 class Graph:
     '''this class implements a graph data structure using an adjacency list representation'''
     '''the graph is represented by a list of vertices and a list of edges
     where the keys are the vertices and the values are lists of
     the vertices that are adjacent to the key vertex, no parallel edges are allowed'''
-    def __init__(self, vertices = set() , edges = list() ) -> None:
+    def __init__(self, graph = {} ,vertices = set() , edges = list() ) -> None:
         
-        self.graph = {}
+        self.graph = graph
         
     def add_vertex_edges(self, vertex :str, edges : list):
         '''add a vertex to the graph'''
         self.graph[vertex]  = edges
          
-    def reconstruct_edges(self):
+    def reconstruct_edges(self) -> list | list:
+        '''gets the edges as list, construct them from the dictionary '''
         edges = []
+        lut = [] # maps edges to their vertices
         for vertex in self.graph:
             for edge in self.graph[vertex]:
-                edges.append([vertex,edge])
-        return edges
+                edges.append(edge)
+                lut.append(vertex)
+        return edges, lut
             
     def contract_edge(self,edge_index):
-        pass
+        edges , lut = self.reconstruct_edges()
+        edge = edges[edge_index]
+        vertex = lut[edge_index]
+        self.graph[vertex + '-' + str(edge) ] = self.graph[vertex] + self.graph[str(edge)]
+        
+    def print_vertices(self):
+        print(self.graph.key())
 
 def main():
     '''read graph from file and contstruct a graph object'''
@@ -39,18 +47,11 @@ def main():
             line = line.split()
             graph.add_vertex_edges((line[0]),line[1:])
             
-    graph.print_nodes()
-    #graph.print_edges()
-    #print('Graph constructed')
-    #graph.plot_graph('Original Graph')
-    #runs = 10000
-    #min_cut = []
-    #min_cut_graph = Graph()
-  
-    #for i in range(runs):
-     #   min_cut_graph.vertices = graph.vertices.copy()
-      #  min_cut_graph.edges = graph.edges.copy()
-       # min_cut.append(min_cut_graph.minimum_cut())
-    #print(f"ran Minimum cut {len(min_cut)} times and the min cut is {min(min_cut)}")
+    #graph.print_nodes()
+    test_graph = Graph(small_graph)
+    print(test_graph.graph)
+    print(test_graph.reconstruct_edges())
+    test_graph.contract_edge(1)
+    print(test_graph.graph)
 if __name__ == "__main__":
     main()
