fc00c33eede4c42a040af2a69cde29fe72fce227
nfarhatclarience <nfarhat@clariencetechnologies.com>
2024-04-26 20:39:29 -0400

    implemented randomization in picking up the edge

--- MinimumCut/MinimumCut.py
+++ MinimumCut/MinimumCut.py
@@ -8,17 +8,17 @@ class Graph (dict):
     '''the graph is represented by a a dictionary where the keys are the vertices and the values are the edges'''
     def __init__(self) -> None:
         super().__init__()
-    def add_vertex(self,vertex) -> None:
+    def add_vertex(self,vertex : str, edges = set()) -> None:
         '''add a vertex to the graph'''
         if vertex not in self:
-            self[vertex] = set()
+            self[vertex] = edges
     def add_edge(self,vertex1,vertex2) -> None:
         '''add an edge to the graph'''
         self[vertex1].add(vertex2)
         self[vertex2].add(vertex1)
-        
+    
     def contract_edge(self,edge_index : int) -> int:
-     pass
+        pass
     def minimum_cut(self):
         '''return the minimum cut of the graph'''
         len_vertices = len(self.vertices)
@@ -33,31 +33,34 @@ def main():
     
     # read input files 
     input_files = [file for file in os.listdir('.') if ('input' in file)]
-    results_valid = list()
-    results_min_cut = list()
+    
     #for file in input_files:
     base_dir = os.getcwd()
     mincut_folder = 'MinimumCut'
     mincut_folder = ''
-    file = 'input_random_9_25.txt'
+    file = 'input_random_1_6.txt'
     file = os.path.join(base_dir,mincut_folder,file)
     print("input ",file)
     g = Graph()
+    
     with open(file) as f:
             for line in f:
                 line = line.split()
-                
-                        
-    cuts = list()
+                g[line[0]] = line[1:]
+    print(len(g.keys()))
+    
+                       
     for run in range(2):
         test_graph = Graph()
-        cuts.append((test_graph.minimum_cut()))
+        
+    result = ''    
     with open(file.replace('input','output')) as f:
         for line in f:
-            results_valid.append(line[0])
-    print("Difference in the Results: ",min(cuts)-int(line[0]))
-    results_min_cut.append(min(cuts))
-        
+            result = line[0]
+    print(result)   
+    keys_index = random.randint(0,len(g.keys())-1)
+    edge_index = random.randint(0,len(g[str(keys_index)])-1)
+    print(g[str(keys_index)][edge_index])    
         
         
        
