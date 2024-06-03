768a8a9150e27658b51e1b75087e85c2888f9221
Nadim Farhat <nadim.farhat@gmail.com>
2024-06-03 10:18:35 -0400

    Seemingly working topological

--- DataStructures/dfs.py
+++ DataStructures/dfs.py
@@ -19,17 +19,19 @@ def dfs_recursive ( graph : Graph, vertex) ->None:
             dfs_recursive(graph,graph[edge])
 
 def topological(graph: Graph , vertex: Vertex) ->None:
-    currentLabel = len(graph)
-    def dfs_recursive ( graph : Graph, vertex) ->None:
+    
+    def dfs_recursive( graph : Graph, vertex :Vertex, currentLabel: list ) ->None:
         vertex.set_explored(True)
         for edge in vertex.edges:
             if (not graph[edge].is_explored()):
-                dfs_recursive(graph,graph[edge])
-        graph[edge].currentLabel = currentLabel
-        currentLabel = currentLabel - 1    
+                dfs_recursive(graph,graph[edge],currentLabel)
+        vertex.currentLabel = currentLabel[0]
+        currentLabel[0] = currentLabel[0] - 1
+        
+    currentLabel = [len(graph)]
     for vertex in graph:
         if (not vertex.is_explored()):
-            dfs_recursive(graph, vertex)    
+            dfs_recursive(graph, vertex, currentLabel)    
                 
     
 
@@ -48,23 +50,11 @@ def main():
     g.add_vertex(number_2)
     g.add_vertex(number_3)
     
+    topological(g,number_0)
     
     for vertex in g:
-        print(vertex.is_explored())
-
-    dfs(g)
-    for vertex in g:
-        print(vertex.is_explored())
-    for vertex in g:
-        vertex.set_explored(False)
-    
-    for vertex in g:
-        print(vertex.is_explored())
-    
-    dfs_recursive(g,g[0])
-    
-    for vertex in g:
-        print(vertex.is_explored())
+        print(vertex.currentLabel)
+  
     
 if __name__ == "__main__":
     main()
