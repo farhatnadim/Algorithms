d0a9e9c4298482ffc9ac2d88173d4d1655c368fb
Nadim Farhat <nadim.farhat@gmail.com>
2024-09-18 11:51:07 -0400

    Part_1 of Lab done
    Refactored original code from Udacity because it s sucked.

--- Udacity/Model/src/main.cpp
+++ Udacity/Model/src/main.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
-using rectangular_grid =vector<vector<uint>>; 
+using rectangular_grid = vector<vector<uint>>; 
 /* TODO: Define a Map class
    Inside the map class, define the mapWidth, mapHeight and grid as a 2D vector
 */
 class Map 
 {
+
 public:
-    Map (uint width, uint height) : mapWidth{width}, mapHeight{height}
+    Map (uint width, uint height, rectangular_grid g) : mapWidth{width}, mapHeight{height},grid{g}
     {
         // initialization fo the grid
         vector<uint> empty_row;
-        empty_row.assign(width,0);
-        for ( auto i = 0 ; i < height ; i++)
-        {
-            grid.push_back(empty_row);
-        }
+        //empty_row.assign(width,0);
+        //grid.assign(height,empty_row);
     }
+
+    // Assignment Constructor 
+
+
     rectangular_grid GetGrid()
     {
         return grid;
@@ -32,18 +34,22 @@ private:
 
 }; // end of class Map
 
-struct point {
-    uint x;
-    uint y;
-};
+
 class Planner
 {
-    private:
+public:
+    Planner( vector<int> s, vector<int> g, int c ): start{s}, goal{g}, cost{c}
+    {
+
+    }
+
+// small class i don't think setter and getters are necessary for now ;
     vector<int> start;
     vector<int> goal;
     int cost;
-    rectangular_grid movements; 
-    rectangular_grid movements_arrows;
+    vector<vector<int>> movements  {{-1,0},{1,0},{0,1},{0,-1}}; 
+    string movements_arrows {{'^'},{'v'},{'<'},{'>'}};
+
 
 };
 /* TODO: Define a Planner class
@@ -84,19 +90,26 @@ void print2DVector(const T & grid)
 int main()
 {
     // Instantiate map and planner objects
-    Map map{6,5};
-    //Planner planner;
-
+    
+    Planner planner({0,0},{4,5},1);
+    rectangular_grid grid = 
+        {{ 0, 1, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 0 },
+        { 0, 0, 0, 1, 1, 0 }};
+    
+    Map map{6,5,grid};
     // Print classes variables
     cout << "Map:" << endl;
     print2DVector(map.GetGrid());
-    /**
+    
     cout << "Start: " << planner.start[0] << " , " << planner.start[1] << endl;
     cout << "Goal: " << planner.goal[0] << " , " << planner.goal[1] << endl;
     cout << "Cost: " << planner.cost << endl;
     cout << "Robot Movements: " << planner.movements_arrows[0] << " , " << planner.movements_arrows[1] << " , " << planner.movements_arrows[2] << " , " << planner.movements_arrows[3] << endl;
     cout << "Delta:" << endl;
     print2DVector(planner.movements);
-    */
+    
     return 0;
 }
